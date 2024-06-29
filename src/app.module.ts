755932d60f5c8fd603c91a4f343c5eb071237f37
Emre Çakmak <ecakmak91@gmail.com>
2024-06-29 14:28:29 +0200

    first step

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
+// import { Module } from '@nestjs/common';
+// import { MongooseModule } from '@nestjs/mongoose';
+
+// @Module({
+//   imports: [
+//     MongooseModule.forRoot('mongodb://localhost:27017/my-nest-project'),
+//   ],
+// })
+// export class AppModule {}
